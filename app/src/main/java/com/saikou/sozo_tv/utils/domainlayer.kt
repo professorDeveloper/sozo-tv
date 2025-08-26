d642a735cec5c55a133f0a504ee3f10266a9da17
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-08-27 01:14:58 +0500

    all navigate to detail func done

--- app/src/main/java/com/saikou/sozo_tv/utils/domainlayer.kt
+++ app/src/main/java/com/saikou/sozo_tv/utils/domainlayer.kt
@@ -19,17 +19,6 @@ import com.saikou.sozo_tv.domain.model.MainModel
 import com.saikou.sozo_tv.domain.model.SearchModel
 import com.saikou.sozo_tv.presentation.screens.home.HomeAdapter
 
-//
-//import android.os.Parcelable
-//import com.ipsat.ipsat_tv.domain.model.BannerItem
-//import com.ipsat.ipsat_tv.domain.model.BannerModel
-//import com.ipsat.ipsat_tv.domain.model.CategoryChannel
-//import com.ipsat.ipsat_tv.domain.model.CategoryChannelItem
-//import com.ipsat.ipsat_tv.domain.model.ChannelResponse
-//import com.ipsat.ipsat_tv.domain.model.Movie
-//import com.ipsat.ipsat_tv.presentation.screens.home.HomeAdapter
-//import kotlinx.parcelize.Parcelize
-//
 
 fun GetRelationsByIdQuery.Media.toDomain(): MainModel {
     return MainModel(
@@ -94,6 +83,7 @@ fun GetCharactersAnimeByIdQuery.Node.toDomain(): Cast {
     )
 }
 
+
 fun List<GenreModel>.toDomain(): CategoryGenre {
     return CategoryGenre("Genres", this.map {
         CategoryGenreItem(
@@ -138,85 +128,3 @@ fun GetAnimeByOnlGenreQuery.Medium.toDomain(): MainModel {
         idMal = it.idMal ?: -1,
     )
 }
-
-////import uz.kinoplus.tv.presentation.screens.home.HomeAdapter.Companion.VIEW_CHANNEL_ITEM
-//
-//fun List<Movie>.toDomain(): BannerModel {
-//    return BannerModel(viewType = HomeAdapter.VIEW_BANNER, data = this.map {
-//        BannerItem(
-//            contentItem = it,
-//        )
-//    })
-//}
-//
-//fun ChannelResponse.toDomain(): CategoryChannel {
-//    return CategoryChannel(
-//        name = "Online TV", list = this.map {
-//            CategoryChannelItem(
-//                content = it
-//            )
-//        }, viewType = HomeAdapter.VIEW_CHANNEL
-//    )
-//}
-////
-////fun ProfileResponse.toDomain(): DisplayUser {
-////    return DisplayUser(
-////        name = this.username,
-////        email = this.email,
-////        phoneNumber = this.phoneNumber,
-////    )
-////}
-////
-////fun ContentItem.toDomain(): BannerItem {
-////    return BannerItem(
-////        thumbnailImageUrl = this.thumbnailImageUrl,
-////        contentItem = this,
-////    )
-////}
-////
-////@Parcelize
-////class ChannelDomain(
-////    val name: String,
-////    val img: String,
-////    val currentShow: String,
-////    val time: String,
-////    override var viewType: Int = HomeAdapter.VIEW_CHANNEL_ITEM
-////) : HomeAdapter.HomeData, Parcelable
-////
-////fun List<CategoryDetailsResponse>.toDomain(): List<CategoryDetails> {
-////    return this.flatMap { response ->
-////        response.data.content.map { content ->
-////            CategoryDetails(
-////                viewType = HomeAdapter.VIEW_CATEGORY_FILMS_ITEM,
-////                id = content.id,
-////                genre = content.genre,
-////                director = content.director ?: Director(id = -1, name = "Unknown"),
-////                rating = content.rating,
-////                isMobileOnly = content.isMobileOnly,
-////                thumbnailImage = content.thumbnailImage,
-////                title = content.title,
-////                isPremiere = content.isPremiere,
-////                description = content.description,
-////                trailerUrl = content.trailerUrl ?: "",
-////                isFree = content.isFree,
-////                isFavorited = content.isFavorited,
-////                releaseDate = content.releaseDate,
-////                widescreenThumbnailImage = content.widescreenThumbnailImage,
-////                slug = content.slug,
-////                isMovie = content.isMovie
-////            )
-////        }
-////    }
-////}
-////
-////fun List<Channel>.toChannelDomain(): List<ChannelDomain> {
-////    return this.map { channel ->
-////        ChannelDomain(
-////            name = channel.name,
-////            img = channel.img,
-////            currentShow = channel.currentShow,
-////            time = channel.time,
-////            viewType = VIEW_CHANNEL_ITEM
-////        )
-////    }
-////}
