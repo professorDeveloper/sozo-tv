d8dcc86e9b1c94275c7879d48df6d5df609d2b05
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-09-05 11:14:21 +0500

    MyAccountPage updated

--- app/src/main/java/com/saikou/sozo_tv/presentation/screens/profile/MyAccountPage.kt
+++ app/src/main/java/com/saikou/sozo_tv/presentation/screens/profile/MyAccountPage.kt
@@ -6,8 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.saikou.sozo_tv.R
+import com.saikou.sozo_tv.databinding.MyAccountPageBinding
 
 
 class MyAccountPage : Fragment() {
+    private var _binding: MyAccountPageBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MyAccountPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
