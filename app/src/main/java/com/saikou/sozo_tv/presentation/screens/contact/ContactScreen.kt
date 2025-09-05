304b2bb89759d79f414b7b290546026da1e5e2ce
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-09-05 11:13:23 +0500

    ContactScreen updated

--- app/src/main/java/com/saikou/sozo_tv/presentation/screens/contact/ContactScreen.kt
+++ app/src/main/java/com/saikou/sozo_tv/presentation/screens/contact/ContactScreen.kt
@@ -6,7 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.saikou.sozo_tv.R
+import com.saikou.sozo_tv.databinding.ContactScreenBinding
 
 class ContactScreen : Fragment() {
+    private var _binding: ContactScreenBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ContactScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
