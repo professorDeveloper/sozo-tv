7b73204c0db73445229837a2288c9a2b151e4ff8
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-01 14:57:34 +0500

    major version released

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.saikou.sozo_tv"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
