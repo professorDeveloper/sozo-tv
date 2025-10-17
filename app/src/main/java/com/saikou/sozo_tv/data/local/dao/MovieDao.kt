bee374c4e63da0c1a300359b5ccb961c685ead21
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-17 16:24:08 +0500

    utils updated

--- app/src/main/java/com/saikou/sozo_tv/data/local/dao/MovieDao.kt
+++ app/src/main/java/com/saikou/sozo_tv/data/local/dao/MovieDao.kt
@@ -10,24 +10,19 @@ import com.saikou.sozo_tv.data.local.entity.AnimeBookmark
 @Dao
 interface MovieDao {
 
-    // Insert a bookmark
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertBookmark(movie: AnimeBookmark)
 
-    // Remove a bookmark
     @Delete
     suspend fun removeBookmark(movie: AnimeBookmark)
 
-    // Check if a movie is bookmarked
     @Query("SELECT EXISTS(SELECT 1 FROM moviebookmark WHERE id = :movieId)")
     suspend fun isBookmarked(movieId: Int): Boolean
 
 
-    // Get all bookmarks
     @Query("SELECT * FROM moviebookmark")
     suspend fun getAllBookmarks(): List<AnimeBookmark>
 
-    // Clear all bookmarks
     @Query("DELETE FROM moviebookmark")
-    suspend fun clearAllBookmarks(): Int // Return the number of rows deleted
+    suspend fun clearAllBookmarks(): Int
 }
